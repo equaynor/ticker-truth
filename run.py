fe8274ac208a157a198df26663e9cc8d9e7dde7d
Emmanuel Quaynor <e.c.quaynor@gmail.com>
2023-12-28 21:34:53 +0100

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,15 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+# User Interaction
+# - User input stock ticker symbols via command line
+# - Tool fetches stock data via api
+
+# Data Analysis and Presentation
+# - Perform basic calculations on the fetched data (e.g., average price, daily change)
+# - Display the analyzed data in a simple, textual format within the command line
+
+# Exception Handling
+# - Implement error handling to manage incorrect or empty inputs and API request failures
+# - Ensure the tool communicates errors effectively to the user for a smoother experience
