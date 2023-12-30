5a13b1665776560979860171046a712584a4557a
Emmanuel Quaynor <e.c.quaynor@gmail.com>
2023-12-30 14:26:47 +0100

    Import yahoo finance api

--- run.py
+++ run.py
@@ -2,6 +2,9 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
+import yahoo_fin.stock_info as si
+
+
 # User Interaction
 # - User input stock ticker symbols via command line
 
